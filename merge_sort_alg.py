6615cfcaf2ca83e7122f4753ba57d5130fe5e927
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-08-24 13:45:40 +0400

    added merge sort alg func

--- merge_sort_alg.py
+++ merge_sort_alg.py
@@ -1,2 +1,42 @@
 from generator_lists import generator_lists
 """Сортировка элементов списка в порядке возрастания с использованием алгоритма сортировки методом слияния"""
+
+
+def merge(left, right):
+
+    if len(left) == 0:
+        return right
+
+    if len(right) == 0:
+        return left
+
+    result = []
+    index_left = index_right = 0
+
+    while len(result) < len(left) + len(right):
+        if left[index_left] <= right[index_right]:
+            result.append(left[index_left])
+            index_left += 1
+        else:
+            result.append(right[index_right])
+            index_right += 1
+
+        if index_right == len(right):
+            result += left[index_left:]
+            break
+
+        if index_left == len(left):
+            result += right[index_right:]
+            break
+
+        return result
+
+
+def merge_sort(numbers: list) -> list:
+
+    if len(numbers) < 2:
+        return numbers
+
+    midpoint = len(numbers) // 2
+
+    return merge(left=merge_sort(numbers[:midpoint]), right=merge_sort(numbers[midpoint:]))
