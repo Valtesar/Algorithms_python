303fda7f6d7e5bd5a469eac019c56ee0ee23012c
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-08-24 13:27:33 +0400

    added generator func + added timeit call

--- insertion_sort_alg.py
+++ insertion_sort_alg.py
@@ -1,3 +1,4 @@
+import random
 """Сортировка элементов списка в порядке возрастания с использованием алгоритма сортировки методом вставки"""
 
 
@@ -10,3 +11,14 @@ def insertion_sort(numbers: list) -> list:
             j -= 1
         numbers[j + 1] = key_item
     return numbers
+
+
+def generator_lists(count, length):
+    """Создаем функцию генератор списков с параметрами:
+        count - колличество списков,
+        length - длинна списков"""
+    start_num = 0
+    end_num = 1000
+    for i in range(count):
+        arr = [random.randint(start_num, end_num) for _ in range(length)]
+        yield arr
--- time_it.py
+++ time_it.py
@@ -1,12 +1,13 @@
 from random import randint
 from timeit import repeat
 from bubble_sort_alg import bubble_sort
+from insertion_sort_alg import insertion_sort
 
 
-def run_sorting_algorithm(algorithm, array):
+def run_sorting_algorithm(algorithm, numbers):
     setup_code = f"from __main__ import {algorithm}" \
         if algorithm != "sorted" else ""
-    stmt = f"{algorithm}({array})"
+    stmt = f"{algorithm}({numbers})"
     times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
 
     print(f"Algorithm: {algorithm}. Minimum execution time: {min(times):.8f}")
@@ -17,6 +18,8 @@ ARRAY_LENGTH = 1000
 
 if __name__ == "__main__":
     array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
-    run_sorting_algorithm(algorithm="sorted", array=array)
-    run_sorting_algorithm(algorithm="bubble_sort", array=array)
+    run_sorting_algorithm(algorithm="sorted", numbers=array)
+    run_sorting_algorithm(algorithm="bubble_sort", numbers=array)
+    run_sorting_algorithm(algorithm="insertion_sort", numbers=array)
+
 
