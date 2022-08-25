099a8070abd292d417d9099b53386087bbc4bb0e
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-08-25 11:19:01 +0400

    added quicksort alg func

---
+++ quicksort_alg.py
@@ -0,0 +1,25 @@
+from random import randint
+from generator_lists import generator_lists
+
+"""Сортировка элементов списка в порядке возрастания с использованием алгоритма сортировки методом слияния"""
+
+
+def quicksort(numbers):
+
+    if len(numbers) < 2:
+        return numbers
+
+    low, same, high = [], [], []
+
+    pivot = numbers[randint(0, len(numbers - 1))]
+
+    for number in numbers:
+        if number < pivot:
+            low.append(number)
+        elif number == pivot:
+            same.append(number)
+        elif number > pivot:
+            high.append(number)
+
+    return quicksort(low) + same + quicksort(high)
+
