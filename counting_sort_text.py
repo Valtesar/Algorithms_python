2829d337640741bcc050377acb9185774cc70ca1
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-08-29 16:44:41 +0400

    fix counting sort alg

--- counting_sort_text.py
+++ counting_sort_text.py
@@ -4,22 +4,22 @@ from string import ascii_letters, digits
 """Сортировка элементов строки в порядке возрастания с использованием алгоритма сортировки методом подсчета"""
 
 
-def counting_sort(numbers):
-    output = [0 for _ in range(len(numbers))]
+def counting_sort_text(string):
+    output = [0 for _ in range(len(string))]
     count = [0 for _ in range(256)]
-    answer = ["" for _ in numbers]
+    answer = ["" for _ in string]
 
-    for i in numbers:
+    for i in string:
         count[ord(i)] += 1
 
     for i in range(256):
         count[i] += count[i - 1]
 
-    for i in range(len(numbers)):
-        output[count[ord(numbers[i])] - 1] = numbers[i]
-        count[ord(numbers[i])] -= 1
+    for i in range(len(string)):
+        output[count[ord(string[i])] - 1] = string[i]
+        count[ord(string[i])] -= 1
 
-    for i in range(len(numbers)):
+    for i in range(len(string)):
         answer[i] = output[i]
     return answer
 
@@ -28,7 +28,7 @@ def counting_sort_test():
     n = randint(5, 10)
     text = ''.join(random.choices(ascii_letters + digits, k=n))
     print(f"Original string is:\n{text}")
-    print(f"Sorted string is:\n{counting_sort(text)}")
+    print(f"Sorted string is:\n{counting_sort_text(text)}")
 
 
 if __name__ == '__main__':
